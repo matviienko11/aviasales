f24e0a33417260fd385221a882d05736ba62b332
Bogdan Matvienko <bohdan.matvienko@faceit-team.com>
2021-08-17 10:40:44 +0300

    commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,11 +20,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'aviasales-ngtest'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('aviasales-ngtest');
-  });
+  // it(`should have as title 'aviasales-ngtest'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('aviasales-ngtest');
+  // });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
