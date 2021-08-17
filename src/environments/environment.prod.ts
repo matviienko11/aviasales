eb83515807b85e07db7956a887b03c4a80b3ddc4
Bogdan Matvienko <bohdan.matvienko@faceit-team.com>
2021-08-17 10:03:18 +0300

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  ID_SEARCH_URL: 'https://front-test.beta.aviasales.ru/search',
+  TICKET_SEARCH_URL: 'https://front-test.beta.aviasales.ru/tickets?searchId='
 };
